cf6fcc2e642386f3606b1cb469199c8b9e62abc6
joey-public <joeypublicgit@gmail.com>
2024-06-29 19:42:27 -0700

    actually woking with no images already in text

--- replaceMdMath.py
+++ replaceMdMath.py
@@ -14,18 +14,19 @@ DEFAULT_IMG_DIR = os.path.expanduser('~/Notes/images/math/')
 def gen_image_link_from_md_math_str(md_file_name:str, n:int, image_dir:str=DEFAULT_IMG_DIR, file_extension='.png'):
     return f'![math_{n}]({image_dir}{md_file_name}_math_{n}.{file_extension})'
 
-md_path = 'test.md'
+md_path = 'temp.md'
 with open(md_path, 'r', encoding='utf-8') as input_file:
     md_str=input_file.read()
 cnt = 0
-
 image_pattern = re.compile(MATH_IMAGE_PATTERN)
 md_math_str_pattern = re.compile(UNLABELED_MATH_PATTERN)
 result = (re.split(MATH_SECTION_PATTERN, md_str))
+
 if len(result)==1: 
     result_str = md_str
 else: 
     result_str = result[1]
+
 math_block_str = ''
 while(True):
     image_match = image_pattern.search(result_str)
@@ -41,4 +42,8 @@ while(True):
     cnt+=1
 
 result_str = result_str + '\n' + MATH_SECTION_PATTERN + '\n' + math_block_str
-print(result_str)
+def save_str_to_file(path:str, text_str:str)->None:
+    with open(path, 'w', encoding='utf-8', errors="xmlcharrefreplace") as output_file:
+        output_file.write(text_str)
+
+save_str_to_file(md_path, result_str)
---
+++ temp_bak.md
@@ -0,0 +1,33 @@
+# Temp
+
+Here is a math eq:
+
+$$
+    a + b = 9
+$$
+
+and more 
+
+$$
+    a - b = 2 
+$$ 
+
+then 
+
+$$
+    a = b + 2
+$$
+
+so that
+
+$$
+    (b+2) + b = 9
+$$
+
+$$
+    2b = 9-2
+$$
+
+$$
+    b = 7/2 = 3.5
+$$
