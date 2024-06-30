f48b1f81f95d8b9b2e58d748e01fc59df2daceb8
joey-public <joeypublicgit@gmail.com>
2024-06-29 21:08:26 -0700

    working on makgin code work on a file with som math images alredy generated

--- replaceMdMath.py
+++ replaceMdMath.py
@@ -4,12 +4,14 @@ import sys
 
 USAGE_MSG = ''
 
-MATH_IMAGE_PATTERN = r'(!\[math_\d\]\(.+\d\.png\))'
+MATH_IMAGE_PATTERN = r'([^\?][^x][^\?]!\[math_\d\]\(.+\d\.png\))'
 UNLABELED_MATH_PATTERN = r'(\$\$[^$]+\$\$)'
 LABELED_MATH_PATTERN = r'(math_\d\n\$\$[^$]+\$\$)'
 MATH_SECTION_PATTERN = r'### Math Blocks'
 
 DEFAULT_IMG_DIR = os.path.expanduser('~/Notes/images/math/')
+
+MATH_IMAGE_N_IDX = 10
 
 def gen_image_link_from_md_math_str(md_file_name:str, n:int, image_dir:str=DEFAULT_IMG_DIR, file_extension='png'):
     return f'![math_{n}]({image_dir}{md_file_name}_math_{n}.{file_extension})'
@@ -20,34 +22,44 @@ with open(md_path, 'r', encoding='utf-8') as input_file:
 cnt = 0
 image_pattern = re.compile(MATH_IMAGE_PATTERN)
 md_math_str_pattern = re.compile(UNLABELED_MATH_PATTERN)
-result = (re.split(MATH_SECTION_PATTERN, md_str))
 
+result = (re.split(MATH_SECTION_PATTERN, md_str))
+math_block_str = ''
+math_scatchpad = ''
 if len(result)==1: 
     result_str = md_str
 else: 
-    math_scratchpad = result[1]
+    old_math_blocks = result[1]
     result_str = result[0]
-
-math_block_str = ''
+#    re.sub(r'!\[math_', r'?x?[math_', result_str)
+print(result_str)
+#TODO: Numbering is off...both image and math_match will be true for  file with images and math
 while(True):
     image_match = image_pattern.search(result_str)
     math_match = md_math_str_pattern.search(result_str)
     if(image_match==None and math_match==None):
+        print('no match!')
         break
     elif math_match==None: #we muct have matched an image
-        img_link = image_match.group(0)
-        n = int(img_link[7])
-        #math_block = re.search(f'math_{}'+
-        pass 
+        print('img match: '+image_match.group(0))
+        n = int(image_match.group(0)[MATH_IMAGE_N_IDX])
+        img_link = '?x?'+gen_image_link_from_md_math_str(md_path[0:-3],  cnt)
+        result_str = md_math_str_pattern.sub(img_link, result_str, count=1)
+        math_scratchpad = re.search(f'(math_{n})', old_math_blocks)
+        math_block_str += re.sub(f'math_{n}', f'math_{cnt}', math_scratchpad.group(0)) 
+        print(result_str)
     else: #we must have matched a math block
-        img_link = gen_image_link_from_md_math_str(md_path[0:-3],  cnt)
+        print('math match: '+math_match.group(0))
+        img_link = '?x?'+gen_image_link_from_md_math_str(md_path[0:-3],  cnt)
         math_block_str += f'math_{cnt}\n'+math_match.group(0)+'\n'
         result_str = md_math_str_pattern.sub(img_link, result_str, count=1)
     cnt+=1
 
 result_str = result_str + '\n' + MATH_SECTION_PATTERN + '\n' + math_block_str
+result_str = re.sub(r'\?x\?', '', result_str)
+print(result_str)
 
 def save_str_to_file(path:str, text_str:str)->None:
     with open(path, 'w', encoding='utf-8', errors="xmlcharrefreplace") as output_file:
         output_file.write(text_str)
-save_str_to_file(md_path, result_str)
+#save_str_to_file(md_path, result_str)
--- temp.md
+++ temp.md
@@ -2,32 +2,51 @@
 
 Here is a math eq:
 
-$$
-    a + b = 9
-$$
+![math_0](/home/joey/Notes/images/math/temp_math_0.png)
 
 and more 
 
-$$
-    a - b = 2 
-$$ 
+![math_1](/home/joey/Notes/images/math/temp_math_1.png) 
 
 then 
 
-$$
-    a = b + 2
-$$
+![math_2](/home/joey/Notes/images/math/temp_math_2.png)
 
 so that
 
+![math_3](/home/joey/Notes/images/math/temp_math_3.png)
+
+![math_4](/home/joey/Notes/images/math/temp_math_4.png)
+
+$$
+    2b + 2 = 9
+$$
+
+![math_5](/home/joey/Notes/images/math/temp_math_5.png)
+
+### Math Blocks
+
+math_0
+$$
+    a + b = 9
+$$
+math_1
+$$
+    a - b = 2 
+$$
+math_2
+$$
+    a = b + 2
+$$
+math_3
 $$
     (b+2) + b = 9
 $$
-
+math_4
 $$
     2b = 9-2
 $$
-
+math_5
 $$
     b = 7/2 = 3.5
 $$
