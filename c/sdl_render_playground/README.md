f4dbe6b8a55059f2741d54e5cbd50b19955c4627
joey-public <joeypublicgit@gmail.com>
2024-08-11 17:36:39 -0700

    messing with SDL rendering

---
+++ c/sdl_render_playground/README.md
@@ -0,0 +1,10 @@
+# SDL Rendering Function Calls
+
+https://wiki.libsdl.org/SDL2/CategoryRender
+
+## Drawing a Circle with SDL
+
+SDL only has a few very basic built in drawing functions. To draw a circle use the `SDL_DrawPoints()` function, and pass an array of `SDL_Point` structures that represent the circle.
+
+Look up the midpoint circle algo
+
--- c/sdl_render_playground/src/main.c
+++ c/sdl_render_playground/src/main.c
@@ -5,7 +5,46 @@
 #include "constants.h"
 #include "init_sdl.h"
 
-void main_loop()
+const SDL_Color C_WHITE= {235,235,235,255};
+const SDL_Color C_GREY= {107,107,107,255};
+const SDL_Color C_BLACK= {20,20,20,255};
+
+typedef struct Line {
+  uint16_t x0, y0, x1, y1;
+}Line;
+
+typedef struct Circle {
+  uint16_t center, radius;
+}Circle;
+
+//void draw_circle(SDL_Renderer *ap_renderer, Circle a_circle, SDL_Color a_color; uint8_t a_num_points)
+//{
+//   ; 
+//}
+
+void set_render_draw_color(SDL_Renderer *ap_renderer, SDL_Color a_c)
+{
+    SDL_SetRenderDrawColor(ap_renderer, a_c.r, a_c.g, a_c.b, a_c.a);
+}
+
+void render(SDL_Renderer* ap_renderer)
+{
+    set_render_draw_color(ap_renderer, C_WHITE);
+    SDL_Rect background = {0,0,SCREEN_WIDTH, SCREEN_HEIGHT};
+    SDL_RenderFillRect(ap_renderer, &background);
+    Line l = {0, 0, SCREEN_WIDTH, SCREEN_HEIGHT};
+    set_render_draw_color(ap_renderer, C_BLACK);
+    //SDL_RenderDrawLine(ap_renderer, l.x0, l.y0, l.x1, l.y1);
+    set_render_draw_color(ap_renderer, C_GREY);
+    //SDL_RenderDrawPoint(ap_renderer, 10,100);
+    set_render_draw_color(ap_renderer, C_BLACK);
+    SDL_Point quad[4] = {{10,10},{10,20},{20,10},{20,20}}; 
+    SDL_RenderDrawPoints(ap_renderer, quad, 4);
+        
+    SDL_RenderPresent(ap_renderer);
+}
+
+void main_loop(SDL_Renderer *ap_renderer)
 {
     uint8_t game_is_running = TRUE;
     while(game_is_running)
@@ -17,6 +56,7 @@ void main_loop()
             printf("The X button was pressed, Quitting the Game \n");
             game_is_running = FALSE;
        }
+       render(ap_renderer);
     }
 }
 int main(void)
@@ -29,6 +69,6 @@ int main(void)
     printf("SDL Setup Failed\n");
     return -1;
   }
-  main_loop();
+  main_loop(main_renderer);
   return 0;
 }
