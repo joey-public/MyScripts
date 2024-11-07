a893f0abdce7ef27f5dcae9f00851118e91aa116
joey-public <joeypublicgit@gmail.com>
2024-11-07 14:40:14 -0800

    trying to use lib for sample drawings

--- python/ShapesPackage/test.py
+++ python/ShapesPackage/test.py
@@ -65,68 +65,35 @@ if __name__ == '__main__':
 
     canvas = pg.Surface((SCREEN_WIDTH,SCREEN_HEIGHT))
     
-    r0 = Rect(50,50,200,50)
-    r1 = Rect(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, 100, 100)
-    r2 = Rect(r0.xm, r0.ym, FILL_WH, FILL_WH) 
+    r0 = Rect(20, 25, 20, 20) # a square
 
-    h_rects = RectArray(Rect(20,20,100,20), 0, 30, 2, 1)
-    v_rects = RectArray(Rect(30,10,20,100), 30, 0, 1, 3)
-    rect_grid = RectGrid(h_rects, v_rects)
+    ra0 = RectArray(Rect(25, 25, r0.w, r0.h), 40, 40, 3, 2)   
+    ra0.moveTo(r0.x1+ra0.dx,r0.y0)
+    ra1 = RectArray(Rect(0, 0, r0.w, ra0.bbox.h), ra0.pitch.x, 0, 1, 3)   
+    ra1.moveTo(ra0.bbox.x1 + ra0.dx, r0.y0)
+    ra2 = RectArray(Rect(0, 0, ra1.bbox.w, r0.h), 0, ra0.pitch.y, 3, 1)
+    ra2.moveTo(ra1.bbox.x1 + ra1.dx, r0.y0)
+    
+    rh = RectArray(Rect(0, 0, ra1.bbox.w, r0.h), 0, ra0.pitch.y, 3, 1)
+    rv = RectArray(Rect(0, 0, r0.w, ra0.bbox.h), ra0.pitch.x, 0, 1, 3)   
+    rg = RectGrid(rh, rv)
+    rg.moveTo(ra2.bbox.x1+ra0.dx,r0.y0)
     
     while running:
         #handle input
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 running = False
-        keys = pg.key.get_pressed()
-        if keys[pg.K_j]:
-            r0.translate(0,-10)
-        if keys[pg.K_k]:
-            r0.translate(0,10)
-        if keys[pg.K_h]:
-            r0.translate(-10, 0)
-        if keys[pg.K_l]:
-            r0.translate(10, 0)
-        if keys[pg.K_r]:
-            r0.rot90(r0.x0, r0.y0)
         #render
         canvas.fill(BG_COLOR)
         draw_rect(canvas, r0, 1, COLOR_0)
-        draw_point(canvas, r0.bl, 3, COLOR_0)
-        draw_rect(canvas, r1, 1, COLOR_2)
-        draw_rect_grid(canvas, rect_grid, 1, 3, COLOR_1)
-        if rm.rect_contains_point(r1, r0.mm):
-            draw_point(canvas, r0.mm, 3, COLOR_RED)
-        if rm.rect_crosses_x(r0, r1.x0):
-            p0 = Point2D(r1.x0, SCREEN_HEIGHT)
-            p1 = Point2D(r1.x0, 0)
-            draw_line(canvas, p0, p1, 2, COLOR_RED)
-        if rm.rect_crosses_x(r0, r1.x1):
-            p0 = Point2D(r1.x1, SCREEN_HEIGHT)
-            p1 = Point2D(r1.x1, 0)
-            draw_line(canvas, p0, p1, 1, COLOR_RED)
-        if rm.rect_crosses_y(r0, r1.y0):
-            p0 = Point2D(0, r1.y0)
-            p1 = Point2D(SCREEN_WIDTH, r1.y0)
-            draw_line(canvas, p0, p1, 2, COLOR_RED)
-        if rm.rect_crosses_y(r0, r1.y1):
-            p0 = Point2D(0, r1.y1)
-            p1 = Point2D(SCREEN_WIDTH, r1.y1)
-            draw_line(canvas, p0, p1, 2, COLOR_RED)
-        if rm.rect_pass_through_rect_x(r0, r1):
-            draw_line(canvas, r1.tl, r1.bl, 2, COLOR_BLUE)
-            draw_line(canvas, r1.tr, r1.br, 2, COLOR_BLUE)
-        if rm.rect_pass_through_rect_y(r0,r1):
-            draw_line(canvas, r1.tl, r1.tr, 2, COLOR_BLUE)
-            draw_line(canvas, r1.bl, r1.br, 2, COLOR_BLUE)
-        if rm.rect_overlaps_rect(r0, r1):
-            draw_rect_fill(canvas, r0, 2, COLOR_0, COLOR_RED)
-            o_rect = rm.get_overlap_rect(r0,r1)
-            draw_rect_fill(canvas, o_rect, 2, COLOR_RED, COLOR_BLUE)
-            fill_rects = rm.get_overlap_rect_array(r0, r1, r2, Point2D(FILL_PITCH, FILL_PITCH), FILL_PITCH)
-            draw_rect_array(canvas, fill_rects, 1, COLOR_0) 
-            draw_point(canvas, o_rect.mm, 3, COLOR_0)
+        draw_rect_array(canvas, ra0, 1, COLOR_0)
+        draw_rect_array(canvas, ra1, 1, COLOR_0)
+        draw_rect_array(canvas, ra2, 1, COLOR_0)
+        draw_rect_grid(canvas, rg, 1, 2, COLOR_0)
+
         screen.blit(pg.transform.flip(canvas, False, True), (0,0))
+#        screen.blit(canvas, (0,0))
         pg.display.flip()
         #handle fsp
         dt = clock.tick(10) / 1000
