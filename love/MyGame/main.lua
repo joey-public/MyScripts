94bb18f940135f6c84d01f25b2d9197762c73e52
joey-public <joeypublicgit@gmail.com>
2024-12-01 13:04:32 -0800

    following sheepsolution Love2D tuts

--- love/MyGame/main.lua
+++ love/MyGame/main.lua
@@ -1,10 +1,39 @@
 function love.load()
-    sprites = love.graphics.newImage('sprites.png')
+    -- Remember: camelCasing!
+    listOfRectangles = {}
 end
 
-function love.draw()
-    love.graphics.print("Hello World", 400, 300)
-    love.graphics.draw(sprites, 0, 0)
+function createRect()
+    rect = {}
+    rect.x = 100
+    rect.y = 100
+    rect.width = 70
+    rect.height = 90
+    rect.speed = 100
+
+    -- Put the new rectangle in the list
+    table.insert(listOfRectangles, rect)
 end
 
+function love.keypressed(key)
+    if key == "j" then 
+        love.event.push("quit")
+    end
+    -- Remember, 2 equal signs (==) for comparing!
+    if key == "space" then
+        createRect()
+    end
+end
+
+function love.update(dt)
+    for i,v in ipairs(listOfRectangles) do
+        v.x = v.x + v.speed * dt
+    end
+end
+
+function love.draw(dt)
+    for i,v in ipairs(listOfRectangles) do
+        love.graphics.rectangle("line", v.x, v.y, v.width, v.height)
+    end
+end
 
